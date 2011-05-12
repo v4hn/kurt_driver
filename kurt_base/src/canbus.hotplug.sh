5dc626822be12f1f7bf9218daf87fd361f80a149
Michael Stypa <mstypa@uos.de>
2011-05-12 16:59:54 +0200

    udev stuff was tested with kurt_base: working
     todo: move setting preferences from canbus script to kurt2socketcan.
    
     Please enter the commit message for your changes. Lines starting

--- kurt_base/src/canbus.hotplug.sh
+++ kurt_base/src/canbus.hotplug.sh
@@ -2,7 +2,10 @@
 
 if [ "${ACTION}" = "add" ]; then
   sleep 5
-  /home/robot/socketcan/can-utils/slcand $( basename ${DEVNAME} )
+  stty -F /dev/ttyUSB0 1000000 line 0 cs8 -onlcr -echo raw min 100 time 2
+  echo -e -n "C\rF\rS8\rO\r" > /dev/ttyUSB0
+  /home/robot/socketcan/can-utils/slcand $( basename ${DEVNAME} ) can0
+  ifconfig can0 up
   exit $?
 elif [ "${ACTION}" = "remove" ]; then
   kill -TERM $(cat /var/run/slcand-$( basename ${DEVNAME} ).pid )
