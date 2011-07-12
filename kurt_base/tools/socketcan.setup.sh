578dd497232e88b7fa7797835255088bf4444187
Martin Günther <mguenthe@uos.de>
2011-07-12 14:04:47 +0200

    comments and minor cleanups of socketcan.setup.sh

--- kurt_base/tools/socketcan.setup.sh
+++ kurt_base/tools/socketcan.setup.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# socketcan.manual.setup.sh:
-# sets options for canusb/peak adapter adapter
-# these commands moved into startup skript in kbs-socketcan package
+# socketcan.setup.sh:
+# Sets options for canusb/peak adapter. These commands moved into startup
+# script in kbs-socketcan debian package (canusb.hotplug.sh). If kbs-socketcan
+# is correctly installed, that script should be run automatically. This script
+# here is just for manually setting up the adapter.
 
-stty -F /dev/ttyUSB0 1000000 line 0 cs8 -onlcr -echo raw min 100 time 2
-echo -e -n "C\rF\rS8\rO\r" > /dev/ttyUSB0
-sudo ~/socketcan/can-utils/slcand ttyUSB0 can0
-sudo ifconfig can0 up
+DEVNAME=/dev/ttyUSB
+IFNAME=can0
+
+stty -F ${DEVNAME} 1000000 line 0 cs8 -onlcr -echo raw min 100 time 2
+echo -e -n "C\rF\rS8\rO\r" > ${DEVNAME}
+sudo /opt/kbs-socketcan/slcand $( basename ${DEVNAME} ) ${IFNAME}
+sudo ifconfig ${IFNAME} up
 
 #peak can usb set speed echo "i 0x0014 e" > /dev/pcan32
